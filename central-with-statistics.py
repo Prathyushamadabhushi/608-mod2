12b0547d66a095e56c5118ff4d2f22ebdcaf1501
Prathyusha <prathyusham0001@gmail.com>
2021-08-30 23:34:35 -0400

    Create central-with-statistics.py

---
+++ central-with-statistics.py
@@ -0,0 +1,19 @@
+import statistics
+values = [47, 95, 88, 73, 88, 84];
+
+# count
+x = len(values)
+print ("Count", len(values))
+
+#sum
+x = sum(values)
+print ("The sum of my_list is", x)
+
+#median
+statistics.median(grades)
+
+#mean
+statistics.mean(grades)
+
+#mode
+statistics.mode(grades);
